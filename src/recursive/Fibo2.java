1e099b15167f726d41be57a22e8c7e92f5135e92
chipang <gnldbs1004@naver.com>
2024-09-22 21:06:10 +0900

    수학 문제에 약한것 좀 해결해야....

--- src/recursive/Fibo2.java
+++ src/recursive/Fibo2.java
@@ -1,4 +1,20 @@
 package recursive;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Fibo2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int n = Integer.parseInt(input);
+        System.out.println(fiboMachine(n));
+    }
+
+    public static long fiboMachine(int n){
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fiboMachine(n-1)+fiboMachine(n-2);
+    }
 }
